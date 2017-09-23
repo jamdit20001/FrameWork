a18722b44f8ea7ce068c05019a410c5af8fe9387
jamdi <jamdit20001@gmail.com>
2017-09-23 14:08:01 -0500

    Selenium

---
+++ facebook/src/main/java/framework/TestHome.java
@@ -0,0 +1,7 @@
+package framework;
+
+/**
+ * Created by jamdi on 9/9/2017.
+ */
+public class TestHome {
+}
--- newyorktimes/src/main/java/pages/HamburgerPage.java
+++
@@ -1,7 +0,0 @@
-package pages;
-
-/**
- * Created by shazeda on 5/25/17.
- */
-public class HamburgerPage {
-}
